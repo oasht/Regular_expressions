506d8595668f5bd3bb374f15d85ea9085e749df1
Olga Shtoppel <olgashtoppel@gmail.com>
2023-03-26 16:52:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using static System.Console;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using System.Text.RegularExpressions;
+
+
+namespace Regular_expressions
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fPath = "address.txt";
+
+
+            /////////////////////////RECORDING////////////////////////////////////
+            
+            using (FileStream fs = new FileStream(fPath, FileMode.Create))
+            {
+                using (StreamWriter sw = new StreamWriter(fs, Encoding.Unicode))
+                {
+                    string address_pattern = @"^([A-Z]([a-z])*\s)+[0-9]+[A-Z]{0,1}(/[0-9]+){0,1},\s[0-9]+$";
+                    WriteLine("Please enter your home address (Exp.: \"Partizana Zheleznyaka 11A, 34\")\n");
+                    string address = ReadLine();
+                    Regex r_address = new Regex(address_pattern);
+                    WriteLine(r_address.IsMatch(address) ? "Correct" : "Incorrect");
+                    if (r_address.IsMatch(address))
+                    {
+                        sw.WriteLine(address);
+                        WriteLine("\nInformation recorded");
+                    }
+                    else { WriteLine("\nInformation was incorrect, try again"); }
+                }
+
+
+
+            }
+
+            /////////////////////////READING////////////////////////////////////
+            
+            using (FileStream fs = new FileStream(fPath, FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(fs, Encoding.Unicode))
+                {
+
+                    WriteLine(sr.ReadToEnd());
+                }
+
+
+                WriteLine("Information was read");
+            }
+        }
+     }
+ }
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Regular expressions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Regular expressions")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f930e5e1-4d0e-4721-a011-f9d73e2c9dd5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
